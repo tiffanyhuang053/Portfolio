0977415308493f1e8d1e0e1b04250e2bb11deea0
Tiffany Huang <tiffanyhuang053@gmail.com>
2026-03-26 17:19:19 -0700

    Refined html documents and added js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// making the img carousel work
+
+let imgBox = document.querySelector("#img-carousel");
+const carouselRight = document.querySelector("#");
+const carouselLeft = document.querySelector("#");
+
+// https://www.youtube.com/watch?v=749ta0nvj8s
+// watch this later to make the carousel :P
+
+carouselRight.addEventListener("click", function() {
+    // imgNum = imgNum();
+    // need to change the image to next one
+})
+
+// function imgChange (imgNum) {
+//     if (imgNum) === 1 {
+//         return 1;
+//     } else if (imgNum) === 2 {
+//         return 2;
+//     } else if (imgNum) === 3 {
+//         return 3;
+//     }
+// }
