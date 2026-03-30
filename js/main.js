fc955f2f2e022931732688ea825e4154934ca32c
Tiffany Huang <tiffany_huang_4@sfu.ca>
2026-03-30 00:11:45 -0700

    Adjusted layout and added interactivity to image carousel

--- js/main.js
+++ js/main.js
@@ -1,16 +1,45 @@
 // making the img carousel work
 
-let imgBox = document.querySelector("#img-carousel");
-const carouselRight = document.querySelector("#");
-const carouselLeft = document.querySelector("#");
+// let imgBox = document.querySelector("#img-carousel");
+const aboutImgs = document.querySelectorAll(".aboutImgs img");
+let aboutImgIndex = 0;
+let aboutIntervalID = null;
+
+function initAboutCarousel() {
+    aboutImgs[aboutImgIndex].classList.add("displayImg");
+}
+
+function showAboutImg(index) {
+    if (index >= aboutImgs.length) {
+        aboutImgIndex = 0;
+    } else if (index < 0) {
+        aboutImgIndex = aboutImgs.length - 1;
+    }
+
+    aboutImgs.forEach(aboutImg => {
+        aboutImg.classList.remove("displayImg");
+    });
+    aboutImgs[aboutImgIndex].classList.add("displayImg");
+}
+
+function prevAboutImg() {
+    aboutImgIndex--;
+    showAboutImg(aboutImgIndex);
+}
+
+function nextAboutImg() {
+    aboutImgIndex++;
+    showAboutImg(aboutImgIndex);
+}
+
+initAboutCarousel();
+
+// const carouselRight = document.querySelector("#");
+// const carouselLeft = document.querySelector("#");
 
 // https://www.youtube.com/watch?v=749ta0nvj8s
 // watch this later to make the carousel :P
 
-carouselRight.addEventListener("click", function() {
-    // imgNum = imgNum();
-    // need to change the image to next one
-})
 
 // function imgChange (imgNum) {
 //     if (imgNum) === 1 {
