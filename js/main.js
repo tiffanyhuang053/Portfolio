a4c18e3814a166ba41096674162a1253b85684f1
Tiffany Huang <tiffany_huang_4@sfu.ca>
2026-04-01 18:39:05 -0700

    Added floating words for 'Behind the Scenes' section and made them interactive

--- js/main.js
+++ js/main.js
@@ -34,19 +34,51 @@ function nextAboutImg() {
 
 initAboutCarousel();
 
-// const carouselRight = document.querySelector("#");
-// const carouselLeft = document.querySelector("#");
+// grabbing words to move in about page
+let floatingWords = document.querySelectorAll(".floating");
+floatingWords.forEach((floatingWord => {
+    dragWord(floatingWord);
+}))
 
-// https://www.youtube.com/watch?v=749ta0nvj8s
-// watch this later to make the carousel :P
+function dragWord (floatingWord) {
+    // getting the words original coordinates to use: https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+    let ogPosition = floatingWord.getBoundingClientRect();
+    let ogY = ogPosition.y;
+    let ogX = ogPosition.x;
 
+    let dragging = false;
+    
+    floatingWord.addEventListener("mousedown", function() {
+        dragging = true;
 
-// function imgChange (imgNum) {
-//     if (imgNum) === 1 {
-//         return 1;
-//     } else if (imgNum) === 2 {
-//         return 2;
-//     } else if (imgNum) === 3 {
-//         return 3;
-//     }
-// }
+        // turning off animations and scaling the text to move
+        floatingWord.style.animation = "none";
+        floatingWord.style.transform = "scale(1.5)";
+        floatingWord.style.position = "fixed";
+        floatingWord.style.left = `${ogX}px`;
+        floatingWord.style.top = `${ogY}px`;
+    });
+
+    document.addEventListener("mousemove", function(e) {
+        if (dragging) {
+            // getting the mouse positions: https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientX
+            floatingWord.style.left = `${e.clientX}px`;
+            floatingWord.style.top = `${e.clientY}px`;
+        } else {
+            return;
+        }
+    });
+
+    floatingWord.addEventListener("mouseup", function() {
+        if (dragging) {
+            dragging = false;
+            floatingWord.style.animation = "";
+            floatingWord.style.animationDelay = "0s";
+            floatingWord.style.position = "absolute"
+            floatingWord.style.left = "";
+            floatingWord.style.top = "";
+        } else {
+            return;
+        }
+    });
+}
